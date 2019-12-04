f57a13214232015e9ce0b3641b36221f153ace2a
Ian Fabricatore <ianfabs@outlook.com>
2019-12-04 04:57:38 +0000

    Converted JS to TS and Moved into src folder

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,42 @@
-import * as utils from "./utils";
-import { isKnownElement } from "./utils";
+import {
+    query,
+    queryAll,
+    append,
+    attach,
+    textNode,
+    style,
+    isKnownElement,
+} from "./utils";
+
+type FunctionComponent = (...props:any[]) => HTMLElement;
+type FC = FunctionComponent;
+type FactoryTagParameter = HTMLElement | FC | String | string;
 
 /**
  * @function
  * The JSX Pragma for vanilla dom for Anise
- * 
- * @param  {string | HTMLElement} tag
- * @param  {} attrs
- * @param  {} children
+ *
+ * @param  {FactoryTagParameter} tag
+ * @param  {any} attrs
+ * @param  {any[]} children
  */
-const element = (tag, attrs = null, ...children = null) => {
+const element = (tag: (HTMLElement | FC | String | string), attrs:any = null, ...children: any[]) => {
     /** The element being created in the element pragma */
-    let _new_element;
+    let _new_element: any;
     if (typeof tag === 'string') _new_element = document.createElement(tag);
-    else if (isKnownElement(tag)) _new_element = tag;
+    else if (isKnownElement(<HTMLElement>tag)) _new_element = tag;
+    // TODO: This might not work, might need to revisit
     else if (tag instanceof Function) {
         const _temp = tag();
         if ( isKnownElement(_temp) ) _new_element = _temp;
         else throw new Error("Anise Error: The function component specified does not return a valid HTMLElement");
     // This else case supports direct text children
-    } else return document.createTextNode(tag);
+    } else return document.createTextNode(<string>tag);
     // Apply attributes to the element being created
     Object.assign(_new_element, attrs);
     // Check if children are specified, and if so render them
     if (children != null) {
-        for (const child of children[Symbol.iterator]()) {
+        for (const child of children) {
             if (isKnownElement(child)) _new_element.appendChild(child);
             /**
              * I'm not certain how necessary this else-if block is.
@@ -35,7 +47,7 @@ const element = (tag, attrs = null, ...children = null) => {
              *    const handleClick = _ => counter++;
              *    return <span>{counter} <button onClick={handleClick}>Click Me</button></span>;
              * };
-             * 
+             *
              * attach(
              *    query("#app"),
              *    <div>
@@ -43,7 +55,7 @@ const element = (tag, attrs = null, ...children = null) => {
              *       <MyFC />
              *    </div>
              * );
-             * 
+             *
              * @description
              * I surmise that this else-if block will never be triggered in this example, because it will compile to the following:
              * @example
@@ -51,7 +63,7 @@ const element = (tag, attrs = null, ...children = null) => {
              *    Anise.element("h1", {}, ["Hello World!"])
              *    Anise.element(MyFC, {}, null)
              * ])
-             * 
+             *
              * After writing this example out I understand that this is necessary.
              */
             else if (child instanceof Function) {
@@ -71,4 +83,13 @@ const element = (tag, attrs = null, ...children = null) => {
 const fragment = () => {};
 
 
-export {element};
+export {
+    element,
+    query,
+    queryAll,
+    append,
+    attach,
+    textNode,
+    style,
+    isKnownElement,
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-/** 
+/**
  * @module anise-utils
  * @author Ian Fabs
  * @copyright 2019; Ian Fabs
@@ -8,65 +8,65 @@
 /**
  * @function
  * A shorthand for `document.querySelector`
- * 
+ *
  * @param  {string} selector Any valid CSS selector.
- * 
+ *
  */
-const query = selector => document.querySelector(selector);
+const query = (selector: string) => document.querySelector(selector);
 /**
  * @function
  * A shorthand for `document.querySelectorAll`
- * 
+ *
  * @param  {string} selector Any valid CSS selector.
- * 
+ *
  */
-const queryAll = selector => document.querySelectorAll(selector);
+const queryAll = (selector: string) => document.querySelectorAll(selector);
 
 /**
  * @function
  * Appends the specified child to the parent, and then returns the parent
- * 
+ *
  * @param  {HTMLElement} parent
  * @param  {HTMLElement} child
- * 
+ *
  * @returns {HTMLElement} The element specified in the parent argument
  */
-const append = (parent, child) => {
-    p.appendChild(c);
-    return p;
+const append: (p: HTMLElement, c: HTMLElement) => HTMLElement = (parent: HTMLElement, child: HTMLElement) => {
+    parent.appendChild(child);
+    return parent;
 };
 
 /**
  * @function
  * Shorthand for `document.createTextNode`
- * 
+ *
  * @param  {string} text The text node contents
- * 
+ *
  * @returns {Text}
  */
-const textNode = text => document.createTextNode(text);
+const textNode = (text: string) => document.createTextNode(text);
 
 /**
  * @function
  * Allows the addition of inline styles.
- * 
+ *
  * @param  {HTMLElement} element The text node contents.
- * 
+ *
  * @returns {HTMLElement} Returns the element passed to it
  */
-const style = element => {
-    return (args, ...strings) => {
+const style = (element: HTMLElement) => {
+    return (args: any[], ...strings: string[]) => {
         // reassembles the template string parts
         let template = args.reduce((a, v, i) => a + v + strings[i]);
         // Remove excess whitespace
-        let lines = template.replace(/[\s]+[\s][\b]?/g, "\n").split("\n").filter(it => it !== "");
+        let lines = template.replace(/[\s]+[\s][\b]?/g, "\n").split("\n").filter( (it: string) => it !== "");
         console.log(lines);
-        lines.forEach( line => {
+        lines.forEach( (line: string) => {
             let keyValuePair = line.split(": ");
             let key = keyValuePair[0]/* .replace(/(?<=\-)[aA-zZ]/g, match => {
                 return match.toUpperCase();
             }) */, value = keyValuePair[1].replace(";","");
-            let temp = {};
+            let temp: any = {};
             temp[key] = value;
             console.log(temp);
             Object.assign(element.style, temp);
@@ -76,7 +76,7 @@ const style = element => {
     }
 }
 
-const isKnownElement = element => {
+const isKnownElement = (element: HTMLElement) => {
     let isHTMLElement = element instanceof HTMLElement;
     let isWebComponent = element instanceof HTMLUnknownElement && (customElements.get( element.tagName ) !== undefined || customElements.get( element.tagName ) !== null);
     return isHTMLElement || isWebComponent;
